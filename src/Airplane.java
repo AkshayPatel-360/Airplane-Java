ee3ca69e80df3d63b9c4378d6f57c922f7862968
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-01-27 13:09:56 -0500

    Initial commit

---
+++ src/Airplane.java
@@ -0,0 +1,31 @@
+public class Airplane {
+
+    private boolean ismotorON = false;
+    public int altitude = 0;
+    private boolean isExploded;
+
+
+
+    public void startMotor()
+    {
+        if (!ismotorON)
+           ismotorON = true;
+    }
+
+
+
+    public void  takeOff()
+    {
+        if (ismotorON & altitude==0 )
+        {
+
+
+        }
+
+    }
+
+
+
+
+
+}
